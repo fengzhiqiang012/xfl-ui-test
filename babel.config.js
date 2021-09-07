81343f79e280a23572914774e778ac147de3e794
fengzhiqiang <fengzhiqiang3@tal.com>
2021-09-07 22:15:14 +0800

    1

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,18 @@
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  // plugins: [
+  //   [
+  //     'import',
+  //     {
+  //       libraryName: 'xfl-ui', // 组件库名称
+  //       style: (name) => {
+  //         const cssName = name.split('/')[2]
+  //         return `xfl-ui/lib/style/${cssName}.css`
+  //       }
+  //     }
+  //   ]
+  // ]
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// import { Button, Switch } from 'xfl-ui'
+import xflUi from 'xfl-ui'
+import "xfl-ui/lib/style/index.css"
+console.log(xflUi)
+import {showText} from "xfl-ui/lib/utils"
+console.log(showText('张三'))
 Vue.config.productionTip = false
-
+// Vue.use(Button).use(Switch)
+Vue.use(xflUi)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
